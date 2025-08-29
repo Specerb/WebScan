8ede772573326d4f60be7e99e0299a2f291b98d7
Specerb <129047905+Specerb@users.noreply.github.com>
2025-08-29 09:54:29 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,9 @@
 
+import axios from "axios";
+
+const API_BASE = "http://localhost:8000";
+
+export async function scanWebsite(url) {
+  const res = await axios.get(`${API_BASE}/scan`, { params: { url } });
+  return res.data;
+}
