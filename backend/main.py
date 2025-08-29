db84d7209e6d7f5c211f6e430ff6386c18f595b5
Specerb <129047905+Specerb@users.noreply.github.com>
2025-08-29 09:51:27 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,13 @@
+from fastapi import FastAPI
+from scanner import check_ssl, check_https, check_headers
+
+app = FastAPI(title="WebScan API")
+
+@app.get("/scan")
+def scan(url: str):
+    results = {}
+    results.update(check_ssl(url))
+    results.update(check_https(url))
+    results.update(check_headers(url))
+    return {"url": url, "results": results}
 
